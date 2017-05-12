16eb7f75315539e724faf21b0b6c06f92a3c9de3
Saferman <s4ferm4n@gmail.com>
2017-05-12 15:02:55 +0800

    complete

--- encodemain.m
+++ encodemain.m
@@ -1,14 +1,32 @@
 clear all;clc;
 N = 32;
-source = zeros(1, N);
-for i=1:N
-    source(i) = rand;
-end
-source = source/sum(source);
-source = sort(source, 'ascend');
+count = 5; % 重复次数
+huffman_v = zeros(5,1);
+fano_v = zeros(5,1);
+shannon_v = zeros(5,1);
 
-[huffman_codeword, huffman_efficiency] = huffmanencode(source);
-[shannon_codeword, shannon_efficiency] = shannonencode(source);
-[fano_codeword, fano_efficiency] = fanoencode(source);
+for k = 1:count
+    source = zeros(1, N);
+    for i=1:N
+        source(i) = rand;
+    end
+    source = source/sum(source);
+    source = sort(source, 'ascend');
+    [huffman_codeword, huffman_efficiency] = huffmanencode(source);
+    [shannon_codeword, shannon_efficiency] = shannonencode(source);
+    [fano_codeword, fano_efficiency] = fanoencode(source);
+    huffman_v(k) = huffman_efficiency;
+    fano_v(k) = fano_efficiency;
+    shannon_v(k) = shannon_efficiency;
+end
+x = 1:count;
+figure;
+plot(x, huffman_v, 'r-', x, fano_v, 'b-', x, shannon_v, 'g-');
+axis([0 6 0.85 1]);   %axis，坐标轴  
+grid on;
+xlabel('次数');
+ylabel('编码效率');
+title('三种编码效率比较');
+legend('霍夫曼编码','费诺编码','香农编码');
 
 
