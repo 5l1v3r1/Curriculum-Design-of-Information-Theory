08732ee49220724f5d9169fe42ec2968daaf3f37
Saferman <s4ferm4n@gmail.com>
2017-05-14 17:15:55 +0800

    delete *.m

--- calentropy.m
+++
@@ -1,9 +0,0 @@
-function entropy = calentropy(source)
-entropy = 0;
-for i=1:length(source)
-    if (source(i) - 0)<=eps(0)
-        continue;
-    end
-    entropy = entropy + -1*source(i)*log2(source(i));
-end
-end
